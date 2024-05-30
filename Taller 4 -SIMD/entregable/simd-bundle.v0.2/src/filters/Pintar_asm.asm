025641f71688f2392eb9e7ded5b60124f01a7f6a
ToniusRetonius <toto.melli074@gmail.com>
2024-05-30 15:44:52 -0300

    Taller 4

--- Taller 4 -SIMD/entregable/simd-bundle.v0.2/src/filters/Pintar_asm.asm
+++ Taller 4 -SIMD/entregable/simd-bundle.v0.2/src/filters/Pintar_asm.asm
@@ -1,13 +1,134 @@
 section .data
 
+Mask_blanco times 4 dd 0xffffffff ; en memoria es : b = ff g = ff r = ff a = ff
+Mask_negro_fila times 4 dd 0xff000000 ; en memoria es : b = 00 g = 00 r = 00 a = ff
+Mask_negro_columna1  dd  0xff000000,0xff000000,0xffffffff,0xffffffff ; los primeros 2 son negros, y los otros 2 blancos
+Mask_negro_columna2  dd  0xffffffff,0xffffffff,0xff000000,0xff000000 ; los primeros 2 son blancos, y los otros 2 negros
+
+; colores de prueba porq el negro no se ve una goma
+Mask_negro_columna3  dd  0xffFFA500,0xffFFA500,0xffffffff,0xffffffff ; los primeros 2 son naranjas, y los otros 2 blancos
+Mask_negro_columna4  dd  0xffffffff,0xffffffff,0xffFFA500,0xffFFA500 ; los primeros 2 son blancos, y los otros 2 naranjas
+Mask_naranja_fila times 4 dd 0xffFFA500 ; en memoria es : b = 00 g = 00 r = 00 a = ff
+
 section .text
 global Pintar_asm
 
 ;void Pintar_asm(unsigned char *src, unsigned char *dst, int width, int height, int src_row_size, int dst_row_size);
+; la estrategia es pintar primero todo blanco : 
+; blanco en rgba = 255,255,255,255
+; luego recorrer (como se trata de una matriz) dos filas superiores, dos inferiores y 4 columnas y poner negro
+; negro en rgba = 0,0,0,255 
 
 Pintar_asm:
+	push rbp
+	mov rbp,rsp
 
+	; cuantas dd tengo que poner el src?
+	xor r10, r10
+	xor r11, r11
+	mov r10d, edx		; r10d = alto
+	mov r11d, ecx		; r11d = ancho
+	imul r10d, r11d		; r10d = edx x ecx (tam en pixeles) 
+
+	; guardamos la dire de rsi 
+	xor r11, r11
+	mov r11, rsi
+
+	; cargamos mask
+	pxor xmm0, xmm0
+	pxor xmm1, xmm1
+	movdqu xmm0, [Mask_blanco]
+
+	; tenemos que poner r10d / 4 (porq pintamos de a 4 pixeles) -veces blanco
+	blanco:	
+	cmp r10d,0
+	je negro
+
+	movdqu [r11], xmm0 
+	add r11, 16			; me muevo 16 bytes en memoria
+	sub r10d, 4			; como pinto de a 4, resto de a 4
+	jmp blanco
 	
+	negro:
+	; pintamos las columnas
+	mov r11, rsi
+	mov r10d, edx		; r10d = alto
+	movdqu xmm1, [Mask_negro_columna3]
+
+	columna1:
+	cmp r10d,0
+	je columna2
+
+	movdqu [r11], xmm1
+	add r11d, r9d		; sumamos el dst_row_size
+	sub r10d, 1
+	jmp columna1
+
+	columna2:
+	mov r11, rsi
+	mov r10d, edx
+	movdqu xmm1, [Mask_negro_columna4]
+	xor r8,	r8
+	mov r8, 4
+	imul r8d, ecx 		; cuantos bytes son el ancho de la imagen? pixeles de ancho x 4
+	sub r8d, 16 		; entonces ahora restamos 16 para estar en la ultima columna de la imagen
+	add r11d, r8d		; offset adecuado
+
+	col2:
+	cmp r10d,0
+	je filas
+
+	movdqu [r11], xmm1
+	add r11d, r9d
+	sub r10d, 1
+	jmp col2
+
+	filas:
+	mov r11, rsi
+	mov r10d, ecx	; ancho 
+	movdqu xmm1, [Mask_naranja_fila]
+	xor r8, r8
+	mov r8, r9
+	add r8, r11 	; offset fila de abajo
+
+	fila1_2:
+	cmp r10d, 0
+	je fila3_4
+
+	movdqu [r11], xmm1
+	movdqu [r8], xmm1
+	add r11, 16
+	add r8, 16
+	sub r10d, 4
+	jmp fila1_2
+
+	fila3_4:
+	mov r11, rsi
+	mov r10d, ecx 	; ancho
+	xor r8, r8
+	mov r8d, edx 	; alto
+	imul r8d, ecx 	; total de pixeles
+	sub r8d, ecx		
+	sub r8d, ecx		; le resto las ultimas 2 filas de pixeles (me quiero parar en altura - 1 al inicio de la columna)
+	xor rax, rax
+	mov rax, 4 		; son 4 bytes x pixel
+	imul r8, rax	; # bytes que tengo q offsetear a rsi
+	add r11, r8
+	xor r8, r8
+	mov r8, 2
+	imul r10d, r8d 	; quiero recorrer dos filas
+
+	f3f4:
+	cmp r10d, 0
+	je fin
+
+	movdqu [r11], xmm1
+	add r11, 16
+	sub r10d, 4
+	jmp f3f4
+
+	fin:
+	pop rbp
 	ret
 	
 
--- Taller 4 -SIMD/entregable/simd-bundle.v0.2/src/filters/temperature_asm.asm
+++ Taller 4 -SIMD/entregable/simd-bundle.v0.2/src/filters/temperature_asm.asm
@@ -1,108 +1,214 @@
+; ARGB
 global temperature_asm
 
-section .data
-borrarA times 4 dd 0x00111111
-borrarRyB times 4 dd 0x00110011
-borrarAyG times 4 dd 0x00110011
-ceros times 4 dd 0x00000000
-mascara db 0xFF,0xff,0xff,14,0xff,0xff,0xFF,12,0xff,0xff,0xFF,10 ,0xff,0xFF,0xFF,8
-tres times 4 dd 3
+section .rodata
+align 16
+    treinta_y_uno: dd 31, 31, 31, 31
+    treinta_y_dos: dd 32, 32, 32, 32
+    noventa_y_cinco: dd 95, 95, 95, 95
+    noventa_y_seis: dd 96, 96, 96, 96
+    ciento_ciencuenta_y_nueve: dd 159, 159, 159, 159
+    ciento_sesenta: dd 160, 160, 160, 160
+    doscientos_veintitres: dd 223, 223, 223, 223
+    doscientos_veinticuatro: dd 224, 224, 224, 224
+    tres: dd 3.0, 3.0, 3.0, 3.0      
+    ;  A R G B A R G B
+    poner_A_en_0: dw 0xFF, 0xFF, 0xFF, 0, 0xFF, 0xFF, 0xFF, 0
+    poner_solo_A: dw 0, 0, 0, 0xFF, 0, 0, 0, 0xFF
+    ;                B  G   R  A
 
-mask_128 db 0,0,0,128,0,0,0,128,0,0,0,128,0,0,0,128
-mask_96 db 0,0,0,96,0,0,0,96,0,0,0,96,0,0,0,96
-mask_255  db 0,0,0,-255,0,0,0,-255,0,0,0,-255,0,0,0,-255
-tresdos times 4 dd 32
+    ;base_caso1: dw 0,    0,    0,   128,   0,    0,    0,   128
+    base_caso1: dw 128,    0,    0,   0,   128,    0,    0,   0
+    ;                     +4*t1                 +4*t2
+    ;sum_caso_1: dw 0,    0,    0,  0xFFFF,   0,    0,    0,  0xFFFF
+    sum_caso_1: dw 0xFFFF,    0,    0,  0,   0xFFFF,    0,    0,  0
+    ;----------------------------------------------------------------------------------------
+
+    ;base_caso2: dw 0,    0,   -128,   255,   0,    0,   -128,   255
+    base_caso2: dw 255, -128, 0, 0, 255, -128, 0, 0
+    ;                +4*t1                 +4*t2          
+    ;sum_caso_2: dw 0,    0,   0xFFFF,    0,   0,    0,   0xFFFF,    0
+    sum_caso_2: dw 0,    0xFFFF,   0,    0,   0,    0xFFFF,   0,    0
+    ;----------------------------------------------------------------------------------------
+
+    ;base_caso3: dw 0,  -384,   255,   639,   0,   -384,   255,   639
+    base_caso3: dw 639,  255,   -384,   0,   639,   255,   -384,   0
+    ;           +4*t1        -4*t1       +4*t2        -4*t2
+    ;sum_caso_3: dw 0,  0xFFFF,    0,    0,   0,   0xFFFF,    0,    0
+    sum_caso_3: dw 0,  0,    0xFFFF,    0,   0,   0,    0xFFFF,    0
+    ;res_caso_3: dw 0,    0,    0,  0xFFFF,   0,    0,    0,  0xFFFF
+    res_caso_3: dw 0xFFFF,    0,    0,  0,   0xFFFF,    0,    0,  0
+    ;----------------------------------------------------------------------------------------
+    ;base_caso4: dw 0,   255,   895,    0,   0,   255,   895,    0
+    base_caso4: dw 0,   895,   255,    0,   0,   895,   255,    0
+    ;                -4*t1                 -4*t2
+    ;res_caso_4: dw 0,    0,   0xFFFF,    0,   0,    0,   0xFFFF,    0
+    res_caso_4: dw 0,    0xFFFF,   0,    0,   0,    0xFFFF,   0,    0
+    ;----------------------------------------------------------------------------------------
+    ;base_caso5: dw 0,  1151,    0,    0,   0,   1151,    0,    0
+    base_caso5: dw 0,  0,    1151,    0,   0,   0,    1151,    0
+    ;           -4*t1                 -4*t2
+    ;res_caso_5: dw 0,  0xFFFF,    0,    0,   0,   0xFFFF,    0,    0
+    res_caso_5: dw 0,  0,    0xFFFF,    0,   0,   0,    0xFFFF,    0
+            
+
 section .text
-;rdi contiene un puntero a la imagen a src
-;rsi contiene un puntero a la imagen dst
-;te lo traes en formato abgr
-;en edx tengo el ancho
-;en ecx tengo el largo
-;en r8d tengo el offset para pasr a la miisma columna pero en la siguiente fila de src
-;en r9d lo mismo pero de dst
+
 ;void temperature_asm(unsigned char *src,
-;              unsigned char *dst,
-;              int width,
-;              int height,
-;              int src_row_size,
-;              int dst_row_size);
+;       unsigned char *dst,
+;       int width,
+;       int height,
+;       int src_row_size,
+;       int dst_row_size)
+;
+; src[rdi], dst[rsi], width[rdx], height[rcx],
+;src_row_size[r8], dst_row_size[r9]
 
 temperature_asm:
-    push rbp
-    mov rbp, rsp
+push rbp
+mov rbp, rsp
 
-    movdqu xmm0, [rdi] ;me traigo de memoria 4 pixeles
-    pand xmm0, [borrarA] ;borro A
-    movdqu xmm1, xmm0 ;copio a xmm1 
-    pslldq xmm1, 1 ;shifteo a izq
-    pand xmm0, [borrarRyB] ;borra r y b de xmm0
-    pand xmm1, [borrarAyG] ;borra a y g de xmm1
-    paddb xmm0, xmm1 ;suma a byte
-    movdqu xmm1, [ceros] ;copialo
-    phaddw xmm0, xmm1 ;t lo tengo todo en la parte alta 
-    ;muevo t con un shuffle
-    
-    pshufb xmm0, [mascara]
-    CVTDQ2PS xmm0, xmm0
-    DIVPS xmm0, [tres] 
-    cvttps2dq xmm0, xmm0
+mov rax, rdx    ; colocamos rdx (ancho) en rax
+mul rcx         ; hacemos rax = rax*rcx(alto)
+mov rcx, rax    ; rcx = ancho*alto
 
-    pxor xmm1, xmm1
-    pxor xmm2, xmm2
-    pxor xmm3, xmm3
-    pxor xmm4, xmm4
-    pxor xmm5, xmm5
-    pxor xmm6, xmm6
-    pxor xmm7, xmm7
-    pxor xmm8, xmm8
-    pxor xmm9, xmm9
-    pxor xmm10, xmm10
+.ciclo:
+    cmp rcx, 0  ; vamps a iterar cada pixel de a 2
+    jle .fin    ; cuando los hayamos visto todos, terminamos el ciclo
 
-    
-    ; en xmm1 guardamos los valores para t < 32 
-    movdqu xmm1, xmm0
-    paddusb xmm1,xmm1
-    paddusb xmm1,xmm1
-    paddusb xmm1,xmm1
-    paddusb xmm1,xmm1           ; t · 4
-    movdqu xmm2, [mask_128]     ; 128 + t · 4
-    paddb xmm1, xmm2
-    psrld xmm1, 8               ; < 0, 0, 128 + t · 4, 0 >
+    ;levantamos 2 pixeles (solo vamos a usar 2, pixeles pasados a 64 bits cada uno)
+    pmovzxbw xmm0, [rdi] 
 
-    
-    
-    ;segundo caso 32<=t < 96
-    ;en xmm3 me traigo el xmm0
-    movdqu xmm3, xmm0
-    PSUBSB xmm3, [tresdos] ;le
-    PADDUSB xmm3, xmm3
-    PADDUSB xmm3, xmm3
-    PADDUSB xmm3, xmm3
-    PADDUSB xmm3, xmm3 ;ya lo reste y 
+    ;primero hacemos una copia de los A de los 2 pixeles
+    movdqu xmm14, xmm0
+    pand xmm14, [poner_solo_A]  ; dejamos solo vivos los A
+    ; vamos a usar xmm14 al final para restaurar los A
 
-    
-    ; en xmm5 guardamos los valores de  96 <= t < 160
-    ; < (t − 96) · 4 , 255 , 255 − (t − 96) · 4 >
-    movdqu xmm2, xmm0
-    movdqu xmm5, [mask_96]
-    
-    psubusb xmm2, xmm5          ; < 0, 0, 0, (t − 96) >
-    
-    paddusb xmm2,xmm2
-    paddusb xmm2,xmm2
-    paddusb xmm2,xmm2
-    paddusb xmm2,xmm2           ; < 0, 0, 0, (t − 96) · 4 >
-    
-    movdqu xmm6, xmm2
-    psrld xmm6, 16              ; < 0, (t − 96) · 4 , 0 , 0 >
-    
-    paddusb xmm2,xmm6           ; < 0, (t − 96) · 4 , 0 , (t − 96) · 4 >
-    
-    movdqu xmm5, [mask_255]
-    paddusb xmm2, xmm5          ;   
+    ; ponemos A en 0 en ambos pixeles
+    pand xmm0, [poner_A_en_0]   
 
+    ;hacemos suma horizontal
+    phaddw xmm0,xmm0
+    phaddw xmm0, xmm0   ; ahora, en xmm0 = | R1+G1+B1 | R2+G2+B2 | R1+G1+B1 | R2+G2+B2 | R1+G1+B1 | R2+G2+B2 | R1+G1+B1 | R2+G2+B2 |
+
+    ; convertimos cada parte en un float, para poder asi dividir por 3
+    pmovzxwd xmm0, xmm0             ; primero pasamos los valores a 32 bits, para poder pasarlos a float, xmm0 = | R1+G1+B1 | R2+G2+B2 | R1+G1+B1 | R2+G2+B2 |
+    cvtdq2ps xmm0, xmm0      ; lo pasamos a float
+    divps xmm0, [tres]       ; dividimos por 3
+    cvtps2dq xmm0, xmm0      ; lo devolvemos a int
+    ; nos quedo xmm0 = | t1 | t2 | t1 | t2 |   * 32 bits
+    pshufd xmm0, xmm0, 0b11_01_10_00 ; lo ponemos asi,  xmm0 = | t1 | t1 | t2 | t2 |
     
 
+    ; temporalmente uso xmm15 para no sobreescribir xmm0
+    movdqu xmm15, [treinta_y_dos]  
+    pcmpgtd xmm15, xmm0        ; 32 >? t
+    movdqu xmm1, xmm15        ; guardo los bools en xmm1
 
-    pop rbp
-    ret
+    movdqu xmm15, [noventa_y_seis]  
+    pcmpgtd xmm15, xmm0        ; 96 >? t
+    movdqu xmm2, xmm15        ; guardo los bools en xmm2
+    movdqu xmm15, xmm0
+    pcmpgtd xmm15, [treinta_y_uno]   ; 31 < t  <-> 32 <= t
+    pand xmm2, xmm15
+
+    movdqu xmm15, [ciento_sesenta]  
+    pcmpgtd xmm15, xmm0        ; 160 >? t
+    movdqu xmm3, xmm15        ; guardo los bools en xmm3
+    movdqu xmm15, xmm0
+    pcmpgtd xmm15, [noventa_y_cinco]   ; 95 < t  <-> 96 <= t
+    pand xmm3, xmm15
+
+    movdqu xmm15, [doscientos_veinticuatro]  
+    pcmpgtd xmm15, xmm0        ; 24 >? t
+    movdqu xmm4, xmm15        ; guardo los bools en xmm4
+    movdqu xmm15, xmm0
+    pcmpgtd xmm15, [ciento_ciencuenta_y_nueve]   ; 159 < t  <-> 160 <= t
+    pand xmm4, xmm15
+
+    movdqu xmm15, xmm0  
+    pcmpgtd xmm15, [doscientos_veintitres]        ; 223 < t  <-> 224 <= t
+    movdqu xmm5, xmm15        ; guardo los bools en xmm5
+
+
+    ; ya tengo las mascaras para el caso 1,2,3,4,5; ahora falta generar todos los casos
+    movdqu xmm6, [base_caso1]
+    movdqu xmm7, [base_caso2]
+    movdqu xmm8, [base_caso3]
+    movdqu xmm9, [base_caso4]
+    movdqu xmm10, [base_caso5]
+
+    ; ahora tomamos xmm0 y pasamos los 4 t´s de 32 bits a 8 de 16.
+    packusdw xmm0, xmm0      ; xmm0 = | t1 | t1 | t2 | t2 | t1 | t1 | t2 | t2 |
+    pshufd xmm0, xmm0, 0b11_01_10_00   ; xmm0 = | t1 | t1 | t1 | t1 | t2 | t2 | t2 | t2 |  lo acomodamos
+    psllw xmm0, 2         ; xmm0 = | 4*t1 | 4*t1 | 4*t1 | 4*t1 | 4*t2 | 4*t2 | 4*t2 | 4*t2 |
+
+    movdqu xmm15, xmm0      ; copio en xmm15 a xmm0. Pues le voy a aplicar varias mascaras
+
+    ; caso 1
+    pand xmm15, [sum_caso_1]  ; xmm15 = | 0 | 0 | 0 |   4*t1 | 0 | 0 | 0 |   4*t2 |
+    paddw xmm6, xmm15     ; xmm6 = | 0 | 0 | 0 | 128 + 4*t1 | 0 | 0 | 0 | 128 + 4*t2 |
+    ;----------------------------------------------------------------------------------------------------
+
+    movdqu xmm15, xmm0  ; reseteo a xmm15
+
+    ; caso 2
+    pand xmm15, [sum_caso_2]  ; xmm15 = | 0 | 0 | 4*t1   | 0 | 0 | 0 | 4*t2   | 0 |
+    paddw xmm7, xmm15     ; xmm6 = | 0 | 0 | 4*t1 - 128 | 255 | 0 | 0 | 4*t2 - 128 | 255 |
+    ;----------------------------------------------------------------------------------------------------
+    
+    movdqu xmm15, xmm0  ; reseteo a xmm15
+
+    ; caso 3
+    pand xmm15, [sum_caso_3]
+    paddw xmm8, xmm15
+    movdqu xmm15, xmm0
+    pand xmm15, [res_caso_3]
+    psubw xmm8, xmm15
+    ;----------------------------------------------------------------------------------------------------
+
+    movdqu xmm15, xmm0  ; reseteo a xmm15
+    
+    ; caso 4
+    pand xmm15, [res_caso_4] 
+    psubw xmm9, xmm15  
+    ;----------------------------------------------------------------------------------------------------
+
+    movdqu xmm15, xmm0  ; reseteo a xmm15
+
+    ; caso 5
+    pand xmm15, [res_caso_5]
+    psubw xmm10, xmm15
+
+
+    ; ya tenemos armados todos los casos, ahora los unimos
+    pand xmm6, xmm1             ; en cada uno, van a quedar los pixeles que hayan pasado su respectivo caso
+    pand xmm7, xmm2
+    pand xmm8, xmm3
+    pand xmm9, xmm4
+    pand xmm10, xmm5
+
+    por xmm6, xmm7              ; ahora los unimos
+    por xmm6, xmm8
+    por xmm6, xmm9
+    por xmm6, xmm10
+
+    ; restauramos A en los 2 pixeles
+    paddw xmm6, xmm14
+
+    ; pasamos el resultado devueltas a 2 pixeles de 32 bits
+    packuswb xmm6, xmm6
+
+    movq [rsi], xmm6
+
+    add rsi, 8 ; avanzo 2 pixeles
+    add rdi, 8 ; avanzo 2 pixeles
+
+    sub rcx, 2
+
+    jmp .ciclo
+
+.fin:
+
+pop rbp
+ret
